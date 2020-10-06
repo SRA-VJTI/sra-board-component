12f5a147265b900c7f05c87cce58f142c709b9fe
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2020-10-06 16:37:46 +0530

    Added function definitions and declarations for buttons

--- include/buttons.h
+++ include/buttons.h
@@ -1,6 +1,14 @@
 #ifndef BUTTONS_H
 #define BUTTONS_H
 
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "esp_err.h"
 #include "pin_defs.h"
+
+esp_err_t enable_buttons();
+int read_button(int pin_number);
 
 #endif
