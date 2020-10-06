f9ab5b7b6d1094756c586f19d1e7ae54aafd2f45
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2020-10-06 16:36:43 +0530

    Added comments to reference code

--- include/motor_driver.h
+++ include/motor_driver.h
@@ -3,4 +3,7 @@
 
 #include "pin_defs.h"
 
+// https://github.com/espressif/esp-idf/tree/master/examples/peripherals/mcpwm/mcpwm_brushed_dc_control
+// look this link for mcpwm example code
+
 #endif
--- include/servo.h
+++ include/servo.h
@@ -3,4 +3,7 @@
 
 #include "pin_defs.h"
 
+// https://github.com/espressif/esp-idf/tree/master/examples/peripherals/mcpwm/mcpwm_servo_control
+// lookup this link for servo control example
+
 #endif
