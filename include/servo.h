87e6aa6b69f9d4b276cbdf9afe500c9b8577f2a8
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2020-10-08 11:54:40 +0530

    Added definitions and macros for servos

--- include/servo.h
+++ include/servo.h
@@ -1,9 +1,31 @@
 #ifndef SERVO_H
 #define SERVO_H
 
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "driver/mcpwm.h"
+#include "soc/mcpwm_periph.h"
+#include "esp_attr.h"
+
+#include "sdkconfig.h"
+#include "esp_log.h"
+#include "esp_err.h"
 #include "pin_defs.h"
 
-// https://github.com/espressif/esp-idf/tree/master/examples/peripherals/mcpwm/mcpwm_servo_control
-// lookup this link for servo control example
+#define SERVO_A_MIN_PULSEWIDTH CONFIG_SERVO_A_MIN_PULSEWIDTH 
+#define SERVO_A_MAX_PULSEWIDTH CONFIG_SERVO_A_MAX_PULSEWIDTH
+#define SERVO_A_MAX_DEGREE     CONFIG_SERVO_A_MAX_DEGREE
+
+#define SERVO_B_MIN_PULSEWIDTH CONFIG_SERVO_B_MIN_PULSEWIDTH 
+#define SERVO_B_MAX_PULSEWIDTH CONFIG_SERVO_B_MAX_PULSEWIDTH
+#define SERVO_B_MAX_DEGREE     CONFIG_SERVO_B_MAX_DEGREE
+
+#define SERVO_C_MIN_PULSEWIDTH CONFIG_SERVO_C_MIN_PULSEWIDTH 
+#define SERVO_C_MAX_PULSEWIDTH CONFIG_SERVO_C_MAX_PULSEWIDTH
+#define SERVO_C_MAX_DEGREE     CONFIG_SERVO_C_MAX_DEGREE
+
+esp_err_t enabled_servo();
+
+int set_angle_servo(int servo_id);
 
 #endif
