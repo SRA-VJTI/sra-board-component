2473d054ffc22109a0e675cba27c6cbdbe6d59ec
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2020-10-09 20:18:07 +0530

    Added comment to function declarations

--- include/servo.h
+++ include/servo.h
@@ -24,8 +24,20 @@
 #define SERVO_C_MAX_PULSEWIDTH CONFIG_SERVO_C_MAX_PULSEWIDTH
 #define SERVO_C_MAX_DEGREE     CONFIG_SERVO_C_MAX_DEGREE
 
+/**
+ * @brief Enables Servo port on the sra board, sets up PWM for the three pins in servo port.
+ * 
+ * @return esp_err_t - returns ESP_OK if servo pins initialised, else it returns ESP_ERR_INVALID_ARG 
+ **/
 esp_err_t enable_servo();
 
+/**
+ * @brief Set the angle of the servos attached to the servo port of SRA Board
+ * 
+ * @param servo_id pin number of the servo whose angle needs to be set
+ * @param degree_of_rotation angle to which the servo must be set, depends on value of MAX_DEGREE macro
+ * @return esp_err_t 
+ */
 esp_err_t set_angle_servo(int servo_id, unsigned int degree_of_rotation);
 
 #endif
