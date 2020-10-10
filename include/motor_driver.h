1661661cc4f8cc91ae45e8547dc0772bc496d38f
luke2103 <lnankamwar_b18@el.vjti.ac.in>
2020-10-10 07:35:10 +0530

    Changes in motor_driver.c and motor_driver.h

--- include/motor_driver.h
+++ include/motor_driver.h
@@ -3,7 +3,27 @@
 
 #include "pin_defs.h"
 
+static int PARALLEL_MODE ;
+
 // https://github.com/espressif/esp-idf/tree/master/examples/peripherals/mcpwm/mcpwm_brushed_dc_control
 // look this link for mcpwm example code
+
+void set_motor_driver_mode(int mode);
+
+
+//Initialise GPIOs for MCPWM
+void mcpwm_gpio_initialize(int PARALLEL_MODE);
+
+//Intialise MCPWM 
+void mcpwm_initialize(int PARALLEL_MODE);
+
+//Functions to control motor motion
+void motor_forward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle);
+
+void motor_backward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle);
+
+void motor_stop(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num);
+
+void read_motor_driver_mode() ;
 
 #endif
