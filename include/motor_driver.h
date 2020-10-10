0f152a215d387a5458dee04e9cb2fbf1f0c7944f
OSSome01 <omkarsargar01@gmail.com>
2020-10-10 11:05:22 +0530

    add function declarations

--- include/motor_driver.h
+++ include/motor_driver.h
@@ -6,4 +6,25 @@
 // https://github.com/espressif/esp-idf/tree/master/examples/peripherals/mcpwm/mcpwm_brushed_dc_control
 // look this link for mcpwm example code
 
+// Set motor driver mode:
+// 1 - Parallel mode
+// 2 - Normal mode
+void set_motor_driver_mode(int mode);
+
+//Initialise GPIOs for MCPWM
+void mcpwm_gpio_initialize();
+
+//Intialise MCPWM 
+void mcpwm_initialize();
+
+//Functions to control motor motion
+void motor_forward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle);
+
+void motor_backward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle);
+
+void motor_stop(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num);
+
+// Read motor driver mode
+void read_motor_driver_mode() ;
+
 #endif
