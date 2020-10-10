f03e37cdb9e5b268eb3b93f92bfb414b8c9a9fa4
luke2103 <lnankamwar_b18@el.vjti.ac.in>
2020-10-10 23:33:07 +0530

    Add function enable_motor_driver() and change data type ofread_motor_driver_mode

--- include/motor_driver.h
+++ include/motor_driver.h
@@ -10,27 +10,49 @@
 // https://github.com/espressif/esp-idf/tree/master/examples/peripherals/mcpwm/mcpwm_brushed_dc_control
 // look this link for mcpwm example code
 
-/** 
- * @brief Sets motor driver working mode
- * @param mode  1 for Parallel mode
- * @param mode  2 for Normal mode
-**/
-void set_motor_driver_mode(int mode);
+/**
+ * @brief Enables Motor driver in Parallel or Normal Mode
+ * 
+ * @param MODE  if MODE = 1 (Parallel Mode) & MODE = 2 (Normal Mode)
+ * @return esp_err_t returns ESP_OK if MCPWM initialised, else it returns ESP_ERR_INVALID_ARG 
+ */
+esp_err_t enable_motor_driver(int MODE);
 
-//Initialise GPIOs for MCPWM
-void mcpwm_gpio_initialize();
-
-//Intialise MCPWM 
-void mcpwm_initialize();
 
 //Functions to control motor motion
+/**
+ * @brief Sets motors in forward direction
+ * 
+ * @param mcpwm_num set MCPWM unit(0-1)
+ * @param timer_num set timer number(0-2) of MCPWM, each MCPWM unit has 3 timers
+ * @param duty_cycle set duty_cycle
+ */
 void motor_forward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle);
 
+
+/**
+ * @brief Sets motors in reverse direction
+ * 
+ * @param mcpwm_num set MCPWM unit(0-1)
+ * @param timer_num set timer number(0-2) of MCPWM, each MCPWM unit has 3 timers
+ * @param duty_cycle set duty_cycle
+ */
 void motor_backward(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num , float duty_cycle);
 
+
+/**
+ * @brief Stops Motors
+ * 
+ * @param mcpwm_num set MCPWM unit(0-1)
+ * @param timer_num set timer number(0-2) of MCPWM, each MCPWM unit has 3 timers
+ */
 void motor_stop(mcpwm_unit_t mcpwm_num, mcpwm_timer_t timer_num);
 
-// Reads motor driver mode
-void read_motor_driver_mode() ;
+/**
+ * @brief Read motor driver mdoe
+ * 
+ * @return int returns mode of motor driver  1 = Parallel mode , 2 = Normal mode
+ */
+int read_motor_driver_mode() ;
 
 #endif
