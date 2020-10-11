ae72f9689f808bfed877eb787718f5a353e61557
ombhilare999 <ombhilare999@gmail.com>
2020-10-11 14:54:02 +0530

    adc header and c file updated

--- include/adc.h
+++ include/adc.h
@@ -1,6 +1,48 @@
 #ifndef ADC_H
-#define ADC_H
+#define 
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+#include "sdkconfig.h"
+#include "logger.h"
+#include "esp_err.h"
 #include "pin_defs.h"
+
+#define DEFAULT_VREF    1100        
+#define NO_OF_SAMPLES   64 
+
+esp_adc_cal_characteristics_t *adc_chars;
+
+/**
+ * @brief Check Vref or Two Point burned into efuse
+ * 
+ **/
+void check_efuse(void);
+
+/**
+ * @brief Configure width and attenuation of ADC 1
+ * 
+ * @param channel[] Array of ADC 1 channels to which LSA is connected.
+ * 
+ * @return esp_err_t - returns ESP_OK if 11dB attenuation and 12 bit width configured to ADC 1, else it returns ESP_ERR_INVALID_ARG.  
+ * 
+ **/
+esp_err_t config_adc1(adc1_channel_t channel[]);
+
+/**
+ * @brief Characterize ADC 1 using either Vref or Two Point
+ * 
+ * @return esp_err_t 
+ **/
+esp_err_t char_adc1();
+
 
 #endif
--- src/adc.c
+++ src/adc.c
@@ -1,1 +1,71 @@
 #include "adc.h"
+
+static const char* TAG_ADC= "adc";
+
+//Check Whether Vref or Two point is burned into efuse
+void check_efuse(void)
+{
+    //Check TP is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) 
+    {
+        ESP_LOGI(TAG_ADC, "eFuse Two Point: Supported");
+    } 
+    else 
+    {
+        ESP_LOGI(TAG_ADC, "eFuse Two Point: NOT supported");
+    }
+
+    //Check Vref is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK)
+    {
+        ESP_LOGI(TAG_ADC, "eFuse Vref: Supported");
+    } 
+    else 
+    {
+        ESP_LOGI(TAG_ADC, "eFuse Vref: NOT supported");
+    }
+}
+
+esp_err_t config_adc1(adc1_channel_t channel[])
+{   
+    esp_err_t err_A; 
+    esp_err_t err_B;
+
+    err_A = adc1_config_width(ADC_WIDTH_BIT_12); //Configure ADC to 12 bit width
+
+	for (int i = 0; i<4; i++)
+	{
+		err_B = adc1_config_channel_atten(channel[i], ADC_ATTEN_DB_11); //Configure ADC to 11dB attenuation
+	}
+
+    if (err_A == ESP_OK && err_B == ESP_OK)
+    {
+        ESP_LOGI(TAG_ADC, "Configured ADC_1 to 12 Bit and 11dB attenuation");
+        return ESP_OK;
+    }
+    else
+    {
+        return ESP_FAIL;
+    }
+
+}
+
+esp_err_t char_adc1()
+{
+	adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, DEFAULT_VREF, adc_chars);
+       
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP)              
+	{   
+        ESP_LOGI(TAG_ADC, "Characterized using Two Point Value");
+    } 
+    else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) 
+    {
+        ESP_LOGI(TAG_ADC, "Characterized using eFuse Vref");
+    } 
+    else 
+    {
+        ESP_LOGI(TAG_ADC, "Characterized using Default Vref");
+    }
+    return ESP_OK;
+}
