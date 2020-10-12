d870ba26e5039956864696bab01b74d6bf1dddc4
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2020-10-12 14:24:50 +0530

    Added bar graph led functionality

--- include/bar_graph.h
+++ include/bar_graph.h
@@ -1,6 +1,17 @@
 #ifndef BAR_GRAPH_H
 #define BAR_GRAPH_H
 
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "driver/gpio.h"
+
+#include "esp_log.h"
+#include "esp_err.h"
 #include "pin_defs.h"
+#include "motor_driver.h"
+
+esp_err_t enable_bar_graph();
+
+esp_err_t set_bar_graph(uint8_t data);
 
 #endif
