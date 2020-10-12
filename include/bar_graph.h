6afe8894226a71c7167aa4f97b16981546a2839b
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2020-10-12 23:40:24 +0530

    Added comments to code, since the logic is tough to understand

--- include/bar_graph.h
+++ include/bar_graph.h
@@ -26,6 +26,8 @@
  * |     normal     |      normal    |   0  |
  * |----------------|----------------|------|
  * 
+ * enabled_bar_graph_flag is set to the value of Mode accordingly.
+ * 
  * @brief enables the bar graph led's gpio pins
  * 
  * @return esp_err_t returns ESP_OK if enabled correctly, and ESP_FAIL if it failed to initialise
@@ -33,6 +35,35 @@
 esp_err_t enable_bar_graph();
 
 /**
+ * The exact working of this is a bit hard to understand, so this is example. Below are the given states of motor drivers
+ * 
+ * Motor Driver A: Normal
+ * Motor Driver B: Off
+ * Mode = 7, so for mode = 7,
+ * mask = bitmask[7] = 0x0F = 00001111
+ * data = 0xAA = 10101010
+ * 
+ * Since, motor driver A is in normal mode and motor driver B is off, we can only use IN5 - IN8 pins of the bar graph LED
+ * 
+ * LED1 <-----> Motor Driver A (IN1) ----- 0 
+ * LED2 <-----> Motor Driver A (IN2) ----- 0
+ * LED3 <-----> Motor Driver A (IN3) ----- 0
+ * LED4 <-----> Motor Driver A (IN4) ----- 0
+ * LED5 <--X--> Motor Driver B (IN5) ----- 1
+ * LED6 <--X--> Motor Driver B (IN6) ----- 1
+ * LED7 <--X--> Motor Driver B (IN7) ----- 1
+ * LED8 <--X--> Motor Driver B (IN8) ----- 1
+ * 
+ * Mask ==> 00001111 => 0x0F
+ * 
+ * Now, if data is 10101010, then even though pins IN1-IN4 are being used by motor driver, it will be set by gpio_set_level, which will
+ * crash the esp32 device. So, we check in the bitmask if the following pin is free or not, if bit is 1, it can be used, if it is 0, then
+ * it can't be used.
+ * 
+ * So, to achieve this we bitwise-and the mask with 0x80(10000000), if result is 0x80, then leftmost bit is 1, else it is 0.
+ * If leftmost bit is 1, means we can use this pin of bar graph, then we do the same thing with data, bitwise-and it with 0x80,
+ * to find the leftmost bit, if it is 1, the pin is set to 1, else 0. Now we left shift the data and mask variable.
+ * 
  * @brief Set the value of bar graph LED
  * 
  * @param data 8bit binary in hexadecimal form, i.e, the pattern that is needed to be shown on the bar graph LED
--- src/bar_graph.c
+++ src/bar_graph.c
@@ -3,55 +3,68 @@
 static const char* TAG_BAR_GRAPH = "bar_graph";
 static int enabled_bar_graph_flag = 0;
 
+// bitmask is a safety mechanism, say if motor driver A is off and motor driver B is in Normal mode, then only
+// pins IN1 - IN4 are usable in the bar graph, as pins IN5 - IN8 are connected to pins of motor driver B and these cannot be used.
+// So, bit mask sets bits of IN5 - IN8 as 0 and that of IN1 - IN4 as 1, because if we set pins used by mcpwm, esp32 will crash or behave oddly. 
 // IN1 is the leftmost bit and IN8 is the rightmost bit
 static uint8_t bitmask[9] = {0x00, 0xFF, 0xFC, 0xF0, 0xCF, 0xCC, 0xC0, 0x0F, 0x0C};
 //                   state    0     1     2     3     4     5     6     7     8
 
+// Just an array of pins used by bar graph led
 static int pin_out[8] = {BG_LED_1, BG_LED_2, BG_LED_3, BG_LED_4, BG_LED_5, BG_LED_6, BG_LED_7, BG_LED_8};
 
 esp_err_t enable_bar_graph()
 {
     uint64_t bit_mask = 0;
+    // motor driver a is off and motor driver b is off, so we can use IN1 - IN8 pins
     if (read_motor_driver_mode(a) == 0 && read_motor_driver_mode(b) == 0)
     {
         bit_mask = (1ULL<<BG_LED_1) | (1ULL<<BG_LED_2) | (1ULL<<BG_LED_3) | (1ULL<<BG_LED_4) | (1ULL<<BG_LED_5) | (1ULL<<BG_LED_6) | (1ULL<<BG_LED_7) | (1ULL<<BG_LED_8); 
         enabled_bar_graph_flag = 1;
     }
+    // motor driver a is off and motor driver b is in parallel mode, so we can use IN1 - IN6 pins
     else if (read_motor_driver_mode(a) == 0 & read_motor_driver_mode(b) == 1)
     {
         bit_mask = (1ULL<<BG_LED_1) | (1ULL<<BG_LED_2) | (1ULL<<BG_LED_3) | (1ULL<<BG_LED_4) | (1ULL<<BG_LED_5) | (1ULL<<BG_LED_6); 
         enabled_bar_graph_flag = 2;
     }
+    // motor driver a is off and motor driver b is in normal mode, so we can use IN1 - IN4 pins
     else if (read_motor_driver_mode(a) == 0 & read_motor_driver_mode(b) == 2)
     {
         bit_mask = (1ULL<<BG_LED_1) | (1ULL<<BG_LED_2) | (1ULL<<BG_LED_3) | (1ULL<<BG_LED_4); 
         enabled_bar_graph_flag = 3;
     }
+    // motor driver a is in parallel mode and motor driver b is off, so we can use IN1, IN2, IN5 - IN8 pins
     else if (read_motor_driver_mode(a) == 1 & read_motor_driver_mode(b) == 0)
     {
         bit_mask = (1ULL<<BG_LED_1) | (1ULL<<BG_LED_2) | (1ULL<<BG_LED_5) | (1ULL<<BG_LED_6) | (1ULL<<BG_LED_7) | (1ULL<<BG_LED_8); 
         enabled_bar_graph_flag = 4;
     }
+    // motor driver a is in parallel mode and motor driver b is in parallel mode, so we can use IN1, IN2, IN5, IN6 pins
     else if (read_motor_driver_mode(a) == 1 & read_motor_driver_mode(b) == 1)
     {
         bit_mask = (1ULL<<BG_LED_1) | (1ULL<<BG_LED_2) | (1ULL<<BG_LED_5) | (1ULL<<BG_LED_6); 
         enabled_bar_graph_flag = 5;
     }
+    // motor driver a is parallel mode and motor driver b is in normal mode, so we can use IN1, IN2 pins
     else if (read_motor_driver_mode(a) == 1 & read_motor_driver_mode(b) == 2)
     {
         bit_mask = (1ULL<<BG_LED_1) | (1ULL<<BG_LED_2); 
         enabled_bar_graph_flag = 6;
     }
+    // motor driver a is in normal mode and motor driver b is off, so we can use IN5 - IN8 pins
     else if (read_motor_driver_mode(a) == 2 & read_motor_driver_mode(b) == 0)
     {
         bit_mask = (1ULL<<BG_LED_5) | (1ULL<<BG_LED_6) | (1ULL<<BG_LED_7) | (1ULL<<BG_LED_8); 
         enabled_bar_graph_flag = 7;
     }
+    // motor driver a is in normal mode and motor driver b is in parallel mode, so we can use IN5, IN6 pins
     else if (read_motor_driver_mode(a) == 2 & read_motor_driver_mode(b) == 1)
     {
         bit_mask = (1ULL<<BG_LED_5) | (1ULL<<BG_LED_6); 
         enabled_bar_graph_flag = 8;
     }
+    // motor driver a is in normal mode and motor driver b is in normal mode, so we cannot use any pins
     else
     {
         enabled_bar_graph_flag = 0;
@@ -87,23 +100,35 @@ esp_err_t enable_bar_graph()
     return err;
 }
 
+
 esp_err_t set_bar_graph(uint8_t data)
 {
+    // store the bitmask according to the mode of bar graph led's
     uint8_t mode_bitmask = bitmask[enabled_bar_graph_flag];
 
+    // since we want to read the 8 bits, we can use a loop 8 times, and left shift 8 times to extract the 8 bits
     for (int i = 0; i < 8; i++)
     {
+        // bitwise-and bitmask with 0x80, so with this we can extract the value of leftmost bit.
+        // Yxxxxxxx & 10000000 == 10000000, if Y = 1 and Yxxxxxxx & 10000000 == 00000000, if Y = 0
+        // if it is 1, means the pin is usable, so we get into the if loop.
         if ((mode_bitmask & 0x80) == 0x80)
         {
+            // We do the same process to extract the leftmost bit of data
+            // If it is 1, then set the pin to 1, else set it to 0
             if ((data & 0x80) == 0x80)
             {
+                // set gpio value to 1
                 gpio_set_level((gpio_num_t)pin_out[i], 1);
             }
             else
             {
+                // set gpio value to 0
                 gpio_set_level((gpio_num_t)pin_out[i], 0);
             }
         }
+        // left-shift bitmask and data once, we do this 8 times to read all the bits
+        // 00110011 << 1 = 01100110
         mode_bitmask = mode_bitmask << 1;
         data = data << 1;
     }
