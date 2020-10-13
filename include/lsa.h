32b5e11a79f842051c659e667eb69f3a7e21f095
Sravan Chiittupalli <sravanchittupalli7@gmail.com>
2020-10-13 19:30:03 +0530

    Added lsa and multisampling code

--- include/lsa.h
+++ include/lsa.h
@@ -2,5 +2,27 @@
 #define LSA_H
 
 #include "pin_defs.h"
+#include "adc.h"
+
+#define NO_OF_SAMPLES   64 
+
+/**
+ * @brief apply multisampling( Average over NO_OF SAMPLE readings ) to get stable readings 
+ 
+ * @param lsa_pin The adc pin on which multisampling is to be applied
+ 
+ * @return adc_reading- The multisampled reading of given lsa_pin
+ **/
+int multisampling(int lsa_pin);
+
+
+/**
+ * @brief Get multisampled lsa readings
+ 
+ * @param lsa_pins[] array that contains all lsa pins
+ 
+ * @return Returns an int array of lsa readings for the given pins.
+**/
+int get_lsa_readings(int lsa_pins[]);
 
 #endif
--- src/lsa.c
+++ src/lsa.c
@@ -1,1 +1,26 @@
 #include "lsa.h"
+
+//int lsa_pins = {LSA_A0, LSA_A1, LSA_A2, LSA_A3};
+
+int multisampling(int lsa_pin)
+{
+    int adc_reading = 0;
+    for( int j = 0 ; j < NO_OF_SAMPLES; j++)
+    {
+        adc_reading += read_adc(lsa_pin);
+    }
+    adc_reading /= NO_OF_SAMPLES;
+    return adc_reading;
+}
+
+
+int get_lsa_readings(int lsa_pins[])
+{
+    int adc_readings[4] = {0};
+
+    for( int i = 0 ; i < 4 ; i++)
+    {
+        adc_readings[i] = multisampling(lsa_pins[i]);
+    }
+    return adc_readings;
+}
