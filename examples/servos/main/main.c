23f5687f079eacc8e5dc90953603efa63c39991e
Gautam Agrawal <gautamnagrawal@gmail.com>
2021-07-14 17:39:56 +0530

    Modified servos example

--- examples/servos/main/main.c
+++ examples/servos/main/main.c
@@ -27,6 +27,39 @@
 #include "freertos/task.h"
 #include "sra_board.h"
 
+// Configuration for Servo A
+static servo_config servo_a = {
+	.servo_pin = SERVO_A,
+	.min_pulse_width = CONFIG_SERVO_A_MIN_PULSEWIDTH,
+	.max_pulse_width = CONFIG_SERVO_A_MAX_PULSEWIDTH,
+	.max_degree = CONFIG_SERVO_A_MAX_DEGREE,
+	.mcpwm_num = MCPWM_UNIT_0,
+	.timer_num = MCPWM_TIMER_0,
+	.gen = MCPWM_OPR_A,
+};
+
+// Configuration for Servo B
+static servo_config servo_b = {
+	.servo_pin = SERVO_B,
+	.min_pulse_width = CONFIG_SERVO_B_MIN_PULSEWIDTH,
+	.max_pulse_width = CONFIG_SERVO_B_MAX_PULSEWIDTH,
+	.max_degree = CONFIG_SERVO_B_MAX_DEGREE,
+	.mcpwm_num = MCPWM_UNIT_0,
+	.timer_num = MCPWM_TIMER_0,
+	.gen = MCPWM_OPR_B,
+};
+
+// Configuration for Servo C
+static servo_config servo_c = {
+	.servo_pin = SERVO_C,
+	.min_pulse_width = CONFIG_SERVO_C_MIN_PULSEWIDTH,
+	.max_pulse_width = CONFIG_SERVO_C_MAX_PULSEWIDTH,
+	.max_degree = CONFIG_SERVO_C_MAX_DEGREE,
+	.mcpwm_num = MCPWM_UNIT_0,
+	.timer_num = MCPWM_TIMER_1,
+	.gen = MCPWM_OPR_A,
+};
+
 void app_main(void)
 {
     // enable servo modules
@@ -36,24 +69,25 @@ void app_main(void)
         //For loop to sweep all servos from 0 degree to 180 degree
         for(int i = 0 ; i < 180 ; i++)
         {   
-            //Servo id : SERVO_A(5) :: SERVO_B(18) :: SERVO_C(19) {DEFINED in pin_defs.h}
-            //Passing servo id and angle to set_angle_servo function 
-            set_angle_servo(SERVO_A,i);
-            set_angle_servo(SERVO_B,i);
-            set_angle_servo(SERVO_C,i);
+
+            //Passing servo configuration and angle to set_angle_servo function 
+            set_angle_servo(&servo_a, i);
+			set_angle_servo(&servo_b, i);
+			set_angle_servo(&servo_c, i);
             vTaskDelay(3/portTICK_PERIOD_MS);   
         }
             
         //For loop to sweep all servos from 180 degree to 0 degree
         for(int i = 180 ; i > 0; i--)
         {   
-            //Passing servo id and angle to set_angle_servo function
-            set_angle_servo(SERVO_A,i);
-            set_angle_servo(SERVO_B,i);
-            set_angle_servo(SERVO_C,i);
+            //Passing servo configuration and angle to set_angle_servo function
+            set_angle_servo(&servo_a, i);
+			set_angle_servo(&servo_b, i);
+			set_angle_servo(&servo_c, i);
             vTaskDelay(3/portTICK_PERIOD_MS);   
         }
         //Delay of one second
         vTaskDelay(1000/portTICK_PERIOD_MS);  
     }
 }
+
