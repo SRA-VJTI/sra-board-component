490656497b04675805e211eef6cff639e6b55d04
gautam-dev-maker <gautamnagrawal@gmail.com>
2022-02-16 00:45:20 +0530

    Rebase the repository

--- include/mpu6050.h
+++ include/mpu6050.h
@@ -49,9 +49,18 @@
 #define I2C_MASTER_RX_BUF_DISABLE 0   /*!< I2C master do not need buffer */
 #define I2C_MASTER_FREQ_HZ 100000     /*!< I2C master clock frequency */
 
+#define MPU6050_RA_PWR_MGMT_1 0x6B
+#define MPU6050_CLOCK_PLL_XGYRO 0x01
+
+#define MPU6050_RA_GYRO_CONFIG 0x1B
+#define MPU6050_GYRO_FS_250 0x00
+
+#define MPU6050_RA_ACCEL_CONFIG 0x1C
+#define MPU6050_ACCEL_FS_2 0x00
+
 #define MPU6050_ADDR 0x68   /*!< slave address for mpu6050 sensor */
-#define ACCE_START_ADD 0x3B /*!< accelerometer start address */
-#define GYRO_START_ADD 0x43 /*!< gyroscope start address */
+#define ACCE_START_ADDR 0x3B /*!< accelerometer start address */
+#define GYRO_START_ADDR 0x43 /*!< gyroscope start address */
 
 #define WRITE_BIT I2C_MASTER_WRITE /*!< I2C master write */
 #define READ_BIT I2C_MASTER_READ   /*!< I2C master read */
