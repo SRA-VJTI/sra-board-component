71e8b5e591f03018b3d33fd2c9a59a5dc411cd51
VedantParanjape <VedantParanjape@users.noreply.github.com>
2022-03-11 19:48:45 +0000

    deploy: 11f964aff5ec9bee7698d03ed9bca183aa8ffd14

--- search/functions_2.js
+++ search/functions_2.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['display_5flsa_0',['display_lsa',['../oled_8h.html#af1bd87b3c18b14b83abaa7a13e87b6f0',1,'display_lsa(line_sensor_array readings, u8g2_t *u8g2):&#160;oled.c'],['../oled_8c.html#af1bd87b3c18b14b83abaa7a13e87b6f0',1,'display_lsa(line_sensor_array readings, u8g2_t *u8g2):&#160;oled.c']]],
   ['display_5fmpu_1',['display_mpu',['../oled_8h.html#a5ff3f1819dfe0d92468bb32a80335e0e',1,'display_mpu(float pitch, float roll, u8g2_t *u8g2):&#160;oled.c'],['../oled_8c.html#a5ff3f1819dfe0d92468bb32a80335e0e',1,'display_mpu(float pitch, float roll, u8g2_t *u8g2):&#160;oled.c']]],
-  ['display_5fsra_5flogo_2',['display_sra_logo',['../oled_8h.html#a3fd9e14359b2890f6a79d6f568916ce3',1,'display_sra_logo(u8g2_t *u8g2):&#160;oled.c'],['../oled_8c.html#a3fd9e14359b2890f6a79d6f568916ce3',1,'display_sra_logo(u8g2_t *u8g2):&#160;oled.c']]],
-  ['display_5fwalle_5flogo_3',['display_walle_logo',['../oled_8h.html#a8de1b63bbf5b8d095394a084ff48504b',1,'display_walle_logo(u8g2_t *u8g2):&#160;oled.c'],['../oled_8c.html#a8de1b63bbf5b8d095394a084ff48504b',1,'display_walle_logo(u8g2_t *u8g2):&#160;oled.c']]]
+  ['display_5fpid_5fvalues_2',['display_pid_values',['../oled_8h.html#afb16632d6f605ccb07d1f21347e86377',1,'display_pid_values(float kp, float ki, float kd, u8g2_t *u8g2):&#160;oled.c'],['../oled_8c.html#afb16632d6f605ccb07d1f21347e86377',1,'display_pid_values(float kp, float ki, float kd, u8g2_t *u8g2):&#160;oled.c']]],
+  ['display_5fsra_5flogo_3',['display_sra_logo',['../oled_8h.html#a3fd9e14359b2890f6a79d6f568916ce3',1,'display_sra_logo(u8g2_t *u8g2):&#160;oled.c'],['../oled_8c.html#a3fd9e14359b2890f6a79d6f568916ce3',1,'display_sra_logo(u8g2_t *u8g2):&#160;oled.c']]],
+  ['display_5fwalle_5flogo_4',['display_walle_logo',['../oled_8h.html#a8de1b63bbf5b8d095394a084ff48504b',1,'display_walle_logo(u8g2_t *u8g2):&#160;oled.c'],['../oled_8c.html#a8de1b63bbf5b8d095394a084ff48504b',1,'display_walle_logo(u8g2_t *u8g2):&#160;oled.c']]]
 ];
