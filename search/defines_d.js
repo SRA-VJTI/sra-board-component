03f8c416db7fd07fa10e26ce148bb62831dad9c2
gautam-dev-maker <gautam-dev-maker@users.noreply.github.com>
2022-06-02 06:35:00 +0000

    deploy: da7547d295985ab136f612f7cd8ad888f26b3289

--- search/defines_d.js
+++ search/defines_d.js
@@ -5,9 +5,10 @@ var searchData=
   ['servo_5fa_2',['SERVO_A',['../pin__defs_8h.html#ad02b2d3f0a65ea90c450ced08da9a4d9',1,'pin_defs.h']]],
   ['servo_5fb_3',['SERVO_B',['../pin__defs_8h.html#a30b46add9f5cd7b5abf873dc9f1009e8',1,'pin_defs.h']]],
   ['servo_5fc_4',['SERVO_C',['../pin__defs_8h.html#a20be66e38dbb25b3000081eecb9b4cb3',1,'pin_defs.h']]],
-  ['str_5',['STR',['../servo_8c.html#a70fb9b3e7d410312baf880c1292f43ba',1,'servo.c']]],
-  ['switch_5f1_6',['SWITCH_1',['../pin__defs_8h.html#afc718c1fcfcf5d762545260289674333',1,'pin_defs.h']]],
-  ['switch_5f2_7',['SWITCH_2',['../pin__defs_8h.html#a62b9d54211060aac86c0ca9b02f7331e',1,'pin_defs.h']]],
-  ['switch_5f3_8',['SWITCH_3',['../pin__defs_8h.html#a760f4fa2340ec8b807fea30119915efa',1,'pin_defs.h']]],
-  ['switch_5f4_9',['SWITCH_4',['../pin__defs_8h.html#a083e2bd040ccdbc6514e13b876ff20f7',1,'pin_defs.h']]]
+  ['servo_5fd_5',['SERVO_D',['../pin__defs_8h.html#abed942b161f2db9f855f196e22459066',1,'pin_defs.h']]],
+  ['str_6',['STR',['../servo_8c.html#a70fb9b3e7d410312baf880c1292f43ba',1,'servo.c']]],
+  ['switch_5f1_7',['SWITCH_1',['../pin__defs_8h.html#afc718c1fcfcf5d762545260289674333',1,'pin_defs.h']]],
+  ['switch_5f2_8',['SWITCH_2',['../pin__defs_8h.html#a62b9d54211060aac86c0ca9b02f7331e',1,'pin_defs.h']]],
+  ['switch_5f3_9',['SWITCH_3',['../pin__defs_8h.html#a760f4fa2340ec8b807fea30119915efa',1,'pin_defs.h']]],
+  ['switch_5f4_10',['SWITCH_4',['../pin__defs_8h.html#a083e2bd040ccdbc6514e13b876ff20f7',1,'pin_defs.h']]]
 ];
