6af8e319364bf1fbe5b75df4b5e11b844f273d3b
Moteen Shah <codeguy.moteen@gmail.com>
2022-06-12 15:30:20 +0530

    update servo pin_defs according to MARIO v2.3

--- include/pin_defs.h
+++ include/pin_defs.h
@@ -99,10 +99,10 @@
 /////////////////////////////
 
 /////////// servos //////////
-#define SERVO_A 5
-#define SERVO_B 18
+#define SERVO_A 15
+#define SERVO_B 5
 #define SERVO_C 19
-#define SERVO_D 15
+#define SERVO_D 18
 /////////////////////////////
 
 #endif
