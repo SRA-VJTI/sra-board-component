98a20a65ea702276b5a951395f9f063e88cd2087
Krishna-13-cyber <krishnanarayanan132002@gmail.com>
2022-12-08 00:23:21 +0530

    Added 5th sensor for the line-following task

--- include/pin_defs.h
+++ include/pin_defs.h
@@ -78,6 +78,7 @@
 #define LSA_A1 35
 #define LSA_A2 36
 #define LSA_A3 39
+#define LSA_A4 32
 /////////////////////////////
 
 /////// motor driver ////////
@@ -86,16 +87,16 @@
 #define MDA_NORMAL_IN_2 33 //MCPWM_UNIT_1 MCPWM0B
 #define MDA_NORMAL_IN_3 25 //MCPWM_UNIT_1 MCPWM1A
 #define MDA_NORMAL_IN_4 26 //MCPWM_UNIT_1 MCPWM1B
-#define MDB_NORMAL_IN_5 27 //MCPWM_UNIT_1 MCPWM2A
-#define MDB_NORMAL_IN_6 14 //MCPWM_UNIT_1 MCPWM2B
-#define MDB_NORMAL_IN_7 12 //MCPWM_UNIT_0 MCPWM2A
-#define MDB_NORMAL_IN_8 13 //MCPWM_UNIT_0 MCPWM2B
+// #define MDB_NORMAL_IN_5 27 //MCPWM_UNIT_1 MCPWM2A
+// #define MDB_NORMAL_IN_6 14 //MCPWM_UNIT_1 MCPWM2B
+// #define MDB_NORMAL_IN_7 12 //MCPWM_UNIT_0 MCPWM2A
+// #define MDB_NORMAL_IN_8 13 //MCPWM_UNIT_0 MCPWM2B
 
 //***** parallel mode *****//
 #define MDA_PARALLEL_IN_1_2 25 //MCPWM_UNIT_1 MCPWM0A
 #define MDA_PARALLEL_IN_3_4 26 //MCPWM_UNIT_1 MCPWM0B
-#define MDB_PARALLEL_IN_5_7 12 //MCPWM_UNIT_1 MCPWM1A
-#define MDB_PARALLEL_IN_6_8 13 //MCPWM_UNIT_1 MCPWM1B
+// #define MDB_PARALLEL_IN_5_7 12 //MCPWM_UNIT_1 MCPWM1A
+// #define MDB_PARALLEL_IN_6_8 13 //MCPWM_UNIT_1 MCPWM1B
 /////////////////////////////
 
 /////////// servos //////////
