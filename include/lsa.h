6913f341b755644455679181274f95a74d69810e
Gautam Agrawal <64469823+gautam-dev-maker@users.noreply.github.com>
2023-01-14 22:04:56 +0530

    Update Pin Configuration with respect to SRA Board 2.4 (#82)
    * Add 5th sensor to LSA
    
    * Add pin definition of 5th sensor
    * Add 5th sensor ADC input
    * Add 5th sensor output in lsa example
    
    Co-authored-by: Krishna Narayanan <krishnanarayanan132002@gmail.com>
    
    * Change pin configuration with respect to latest SRA Board
    
    * Remove redundant pin configuration and API of Motor B
    * Change bar_graph conditions for Motor B
    
    Co-authored-by: Krishna Narayanan <krishnanarayanan132002@gmail.com>
    
    * Corrects bug in adc_reading array size
    
    Co-authored-by: Krishna Narayanan <krishnanarayanan132002@gmail.com>

--- include/lsa.h
+++ include/lsa.h
@@ -39,9 +39,10 @@ typedef union line_sensor_array
         int A1;
         int A2;
         int A3;
+        int A4;
     } lsa;
 
-    int adc_reading[4];
+    int adc_reading[5];
 } line_sensor_array;
 
 /**
--- include/pin_defs.h
+++ include/pin_defs.h
@@ -56,14 +56,14 @@
 #endif
 
 //////// bar graph //////////
-#define BG_LED_1 32
+#define BG_LED_1 12
 #define BG_LED_2 33
 #define BG_LED_3 25
 #define BG_LED_4 26
 #define BG_LED_5 27
 #define BG_LED_6 14
-#define BG_LED_7 12
-#define BG_LED_8 13
+#define BG_LED_7 13
+#define BG_LED_8 23
 /////////////////////////////
 
 ////////// switches //////////
@@ -78,24 +78,19 @@
 #define LSA_A1 35
 #define LSA_A2 36
 #define LSA_A3 39
+#define LSA_A4 32
 /////////////////////////////
 
 /////// motor driver ////////
 //****** normal mode ******//
-#define MDA_NORMAL_IN_1 32 //MCPWM_UNIT_1 MCPWM0A
+#define MDA_NORMAL_IN_1 12 //MCPWM_UNIT_1 MCPWM0A
 #define MDA_NORMAL_IN_2 33 //MCPWM_UNIT_1 MCPWM0B
 #define MDA_NORMAL_IN_3 25 //MCPWM_UNIT_1 MCPWM1A
 #define MDA_NORMAL_IN_4 26 //MCPWM_UNIT_1 MCPWM1B
-#define MDB_NORMAL_IN_5 27 //MCPWM_UNIT_1 MCPWM2A
-#define MDB_NORMAL_IN_6 14 //MCPWM_UNIT_1 MCPWM2B
-#define MDB_NORMAL_IN_7 12 //MCPWM_UNIT_0 MCPWM2A
-#define MDB_NORMAL_IN_8 13 //MCPWM_UNIT_0 MCPWM2B
 
 //***** parallel mode *****//
 #define MDA_PARALLEL_IN_1_2 25 //MCPWM_UNIT_1 MCPWM0A
 #define MDA_PARALLEL_IN_3_4 26 //MCPWM_UNIT_1 MCPWM0B
-#define MDB_PARALLEL_IN_5_7 12 //MCPWM_UNIT_1 MCPWM1A
-#define MDB_PARALLEL_IN_6_8 13 //MCPWM_UNIT_1 MCPWM1B
 /////////////////////////////
 
 /////////// servos //////////
